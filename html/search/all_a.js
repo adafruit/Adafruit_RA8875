289cb34265cf4d63f0ca19f6aae2cb15b008ff85
Travis CI <travis@travis-ci.org>
2019-04-18 15:40:58 +0000

    Deploy code docs to GitHub Pages Travis build: 12
    Commit: 6386724212852ab51384b64fc1223b7de330ff3b

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['ra8875_5f480x128',['RA8875_480x128',['../_adafruit___r_a8875_8h.html#aadfa25dab72d848e1d3c1562fec77a0cafd96e457f0d4d8a8476fd43a4a84eb90',1,'Adafruit_RA8875.h']]],
   ['ra8875_5f480x272',['RA8875_480x272',['../_adafruit___r_a8875_8h.html#aadfa25dab72d848e1d3c1562fec77a0ca03ea14912b4654ab8c87b09076a43923',1,'Adafruit_RA8875.h']]],
+  ['ra8875_5f480x80',['RA8875_480x80',['../_adafruit___r_a8875_8h.html#aadfa25dab72d848e1d3c1562fec77a0ca84c1e8eff5ddb4f858a9712dd400babc',1,'Adafruit_RA8875.h']]],
   ['ra8875_5f800x480',['RA8875_800x480',['../_adafruit___r_a8875_8h.html#aadfa25dab72d848e1d3c1562fec77a0ca57f93240e42468602ddfea97d0b26f7b',1,'Adafruit_RA8875.h']]],
   ['ra8875_5fblack',['RA8875_BLACK',['../_adafruit___r_a8875_8h.html#a937e10e550c933a8bfb74ae4491bcd99',1,'Adafruit_RA8875.h']]],
   ['ra8875_5fblue',['RA8875_BLUE',['../_adafruit___r_a8875_8h.html#aa5efb0212e002298145fd6197c0e1aa2',1,'Adafruit_RA8875.h']]],
